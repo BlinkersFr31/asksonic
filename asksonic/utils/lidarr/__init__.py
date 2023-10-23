263dd024fdc8f1c3e3e6100324207a5e047d61fb
BlinkersFr31 <36617780+BlinkersFr31@users.noreply.github.com>
2023-10-23 16:18:04 +0200

    Lidarr init

---
+++ asksonic/utils/lidarr/__init__.py
@@ -0,0 +1,26 @@
+from os import getenv
+from .api import Liadarr
+
+#subsonic_url = getenv('ASKS_SUBSONIC_URL', '')
+#subsonic_user = getenv('ASKS_SUBSONIC_USER', '')
+#subsonic_pass = getenv('ASKS_SUBSONIC_PASS', '')
+#subsonic_serverpath = getenv('ASKS_SUBSONIC_SERVERPATH', '/rest')
+#subsonic_port = int(getenv('ASKS_SUBSONIC_PORT', 443))
+#subsonic_apiversion = getenv('ASKS_SUBSONIC_APIVERSION', API_VERSION)
+#subsonic_appname = getenv('ASKS_SUBSONIC_APPNAME', 'py-sonic')
+#extra_secret = getenv('ASKS_EXTRA_SECRET')
+
+#if any(x == '' for x in [subsonic_url, subsonic_user, subsonic_pass]):
+#    raise RuntimeError('Subsonic login information is missing from env')
+
+
+liadarr = Lidarr(
+    #subsonic_url,
+    #subsonic_user,
+    #subsonic_pass,
+    #subsonic_port,
+    #subsonic_serverpath,
+    #subsonic_apiversion,
+    #subsonic_appname,
+    #extra_secret
+)
